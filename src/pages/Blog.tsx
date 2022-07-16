b70e2984c59ce12fa7e43db9536cdc7e5057834f
Thien Nguyen <thiennguyen@Neivs-MacBook-Air.local>
2022-07-16 11:25:27 +0700

    feat(blog-page-update): Update new UI for blog detail and update language for the application

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,19 +1,27 @@
-import { Card } from 'antd';
+import { CalendarOutlined } from '@ant-design/icons';
+import { Button, Card } from 'antd';
 import { Link } from 'react-router-dom';
 
 function BlogPage() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="mt-6 columns-3 gap-8">
       <Card
+        headStyle={{
+          fontWeight: 600,
+        }}
         title="Default size card"
-        extra={<Link to="/blogs/123">Detail</Link>}
-        style={{ width: '100%' }}
+        extra={
+          <Button type="primary">
+            <Link to="/blogs/123">Chi tiết</Link>
+          </Button>
+        }
+        style={{ width: '100%', marginBottom: 38 }}
       >
         <div className="flex flex-col gap-4">
           <div>
             <img
               src="https://picsum.photos/200/300"
-              className="h-96 w-full object-cover object-center"
+              className=" h-60 w-full object-cover object-center"
             />
           </div>
           <div className="flex items-center gap-2">
@@ -23,8 +31,10 @@ function BlogPage() {
             />
             <div className="font-bold">User name</div>
           </div>
-          <div className=" text-color-gray-40">Date: 22/2/2222</div>
-          <p>
+          <div className=" text-color-gray-40">
+            <CalendarOutlined /> 22/2/2222
+          </div>
+          <p className=" h-32">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi
             laudantium laborum ad reprehenderit iure consectetur. Ratione eum
             necessitatibus, natus reprehenderit aliquid itaque, libero, quo
@@ -33,15 +43,22 @@ function BlogPage() {
         </div>
       </Card>
       <Card
+        headStyle={{
+          fontWeight: 600,
+        }}
         title="Default size card"
-        extra={<Link to="/blogs/123">Detail</Link>}
-        style={{ width: '100%' }}
+        extra={
+          <Button type="primary">
+            <Link to="/blogs/123">Chi tiết</Link>
+          </Button>
+        }
+        style={{ width: '100%', marginBottom: 38 }}
       >
         <div className="flex flex-col gap-4">
           <div>
             <img
               src="https://picsum.photos/200/300"
-              className="h-96 w-full object-cover object-center"
+              className=" h-60 w-full object-cover object-center"
             />
           </div>
           <div className="flex items-center gap-2">
@@ -51,8 +68,10 @@ function BlogPage() {
             />
             <div className="font-bold">User name</div>
           </div>
-          <div className=" text-color-gray-40">Date: 22/2/2222</div>
-          <p>
+          <div className=" text-color-gray-40">
+            <CalendarOutlined /> 22/2/2222
+          </div>
+          <p className=" h-32">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi
             laudantium laborum ad reprehenderit iure consectetur. Ratione eum
             necessitatibus, natus reprehenderit aliquid itaque, libero, quo
@@ -61,15 +80,22 @@ function BlogPage() {
         </div>
       </Card>
       <Card
+        headStyle={{
+          fontWeight: 600,
+        }}
         title="Default size card"
-        extra={<Link to="/blogs/123">Detail</Link>}
-        style={{ width: '100%' }}
+        extra={
+          <Button type="primary">
+            <Link to="/blogs/123">Chi tiết</Link>
+          </Button>
+        }
+        style={{ width: '100%', marginBottom: 38 }}
       >
         <div className="flex flex-col gap-4">
           <div>
             <img
               src="https://picsum.photos/200/300"
-              className="h-96 w-full object-cover object-center"
+              className=" h-60 w-full object-cover object-center"
             />
           </div>
           <div className="flex items-center gap-2">
@@ -79,8 +105,121 @@ function BlogPage() {
             />
             <div className="font-bold">User name</div>
           </div>
-          <div className=" text-color-gray-40">Date: 22/2/2222</div>
-          <p>
+          <div className=" text-color-gray-40">
+            <CalendarOutlined /> 22/2/2222
+          </div>
+          <p className=" h-32">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi
+            laudantium laborum ad reprehenderit iure consectetur. Ratione eum
+            necessitatibus, natus reprehenderit aliquid itaque, libero, quo
+            officia nihil exercitationem consequatur dolore!
+          </p>
+        </div>
+      </Card>
+      <Card
+        headStyle={{
+          fontWeight: 600,
+        }}
+        title="Default size card"
+        extra={
+          <Button type="primary">
+            <Link to="/blogs/123">Chi tiết</Link>
+          </Button>
+        }
+        style={{ width: '100%', marginBottom: 38 }}
+      >
+        <div className="flex flex-col gap-4">
+          <div>
+            <img
+              src="https://picsum.photos/200/300"
+              className=" h-60 w-full object-cover object-center"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <img
+              src="https://picsum.photos/200/300"
+              className="h-10 w-10 rounded-full object-cover object-center"
+            />
+            <div className="font-bold">User name</div>
+          </div>
+          <div className=" text-color-gray-40">
+            <CalendarOutlined /> 22/2/2222
+          </div>
+          <p className=" h-32">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi
+            laudantium laborum ad reprehenderit iure consectetur. Ratione eum
+            necessitatibus, natus reprehenderit aliquid itaque, libero, quo
+            officia nihil exercitationem consequatur dolore!
+          </p>
+        </div>
+      </Card>
+      <Card
+        headStyle={{
+          fontWeight: 600,
+        }}
+        title="Default size card"
+        extra={
+          <Button type="primary">
+            <Link to="/blogs/123">Chi tiết</Link>
+          </Button>
+        }
+        style={{ width: '100%', marginBottom: 38 }}
+      >
+        <div className="flex flex-col gap-4">
+          <div>
+            <img
+              src="https://picsum.photos/200/300"
+              className=" h-60 w-full object-cover object-center"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <img
+              src="https://picsum.photos/200/300"
+              className="h-10 w-10 rounded-full object-cover object-center"
+            />
+            <div className="font-bold">User name</div>
+          </div>
+          <div className=" text-color-gray-40">
+            <CalendarOutlined /> 22/2/2222
+          </div>
+          <p className=" h-32">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi
+            laudantium laborum ad reprehenderit iure consectetur. Ratione eum
+            necessitatibus, natus reprehenderit aliquid itaque, libero, quo
+            officia nihil exercitationem consequatur dolore!
+          </p>
+        </div>
+      </Card>
+      <Card
+        headStyle={{
+          fontWeight: 600,
+        }}
+        title="Default size card"
+        extra={
+          <Button type="primary">
+            <Link to="/blogs/123">Chi tiết</Link>
+          </Button>
+        }
+        style={{ width: '100%', marginBottom: 38 }}
+      >
+        <div className="flex flex-col gap-4">
+          <div>
+            <img
+              src="https://picsum.photos/200/300"
+              className=" h-60 w-full object-cover object-center"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <img
+              src="https://picsum.photos/200/300"
+              className="h-10 w-10 rounded-full object-cover object-center"
+            />
+            <div className="font-bold">User name</div>
+          </div>
+          <div className=" text-color-gray-40">
+            <CalendarOutlined /> 22/2/2222
+          </div>
+          <p className=" h-32">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quasi
             laudantium laborum ad reprehenderit iure consectetur. Ratione eum
             necessitatibus, natus reprehenderit aliquid itaque, libero, quo
